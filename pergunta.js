de1dd7731711fb175dd9a73a2a47ea45f1f54084
Cauã Rodolpho <Cauarodolpho@gmail.com>
2024-10-24 06:28:53 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,25 @@
+criaCartao (
+'Programação',
+'O que é CSS',
+'CSS é uma linguagem de estilização'
+
+)
+
+criaCartao (
+    'Geografia',
+    'Qual é a Capital da França?',
+    'A Capital da França é París'
+)    
+   
+criaCartao (
+    'Programação',
+    'O que é Python',
+    'é uma linguagem de programação'
+)  
+
+criaCartao (
+    'Língua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é Hi (RAI)'
+
+)
